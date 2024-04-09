892d284954141dd0ccf944e1827d0a18f93091a6
bbageon <rjsdn4403@gmail.com>
2024-04-10 00:48:57 +0900

    websocket 통신완료
    websocket 통신완료

--- Backend/test2.py
+++ Backend/test2.py
@@ -1,21 +1,71 @@
-import websockets
 import asyncio
-import socket
+import cv2
+import numpy as np
+import websockets
+from ultralytics import YOLO
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+# Load the YOLOv8 model
+model = YOLO('yolov8n.pt')
 
-    greeting = f"안녕하세요, {name}!"
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+# 이미지 중심 좌표 (0,0)
+img_center_x = 112
+img_center_y = 112
 
-# 호스트 이름을 IP 주소로 변환
-host_ip = socket.gethostbyname(socket.gethostname())
-print("호스트 ip :", host_ip)
+async def receive_image():
+    uri = "ws://10.1.169.172:5000"  # 서버의 주소 및 포트
+    async with websockets.connect(uri) as websocket:
+        while True:
+            # 서버로부터 이미지 데이터 수신
+            data = await websocket.recv()
+            # print("데이터 수신 완료")
 
-start_server = websockets.serve(hello, host_ip, 8000)
+            # 수신된 데이터를 이미지로 디코딩
+            nparr = np.frombuffer(data, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-print("서버 실행완료")
-asyncio.get_event_loop().run_forever()
+            # 이미지를 화면에 표시
+            cv2.imshow("Received Image", img)
+            cv2.waitKey(1)
+            
+            # YOLO 모델 구동
+            results = model(img, stream=True)
+            for result in results:
+                boxes = result.boxes.xyxy  # Boxes object for bounding box outputs
+                # masks = result.masks  # Masks object for segmentation masks outputs
+                # keypoints = result.keypoints  # Keypoints object for pose outputs
+                # probs = result.probs  # Probs object for classification outputs
+                # print("결과값 : " , boxes, probs)
+                # result.show()  # display to screen
+                # result.save(filename='result.jpg')
+                print("@#@#",boxes)
+            
+                if len(boxes) > 0:    
+                    for box in boxes:
+                        x1, y1, x2, y2 = box
+                        box_center_x = round((((x1 + x2) / 2) / 224).item(), 6)
+                        box_center_y = round((((y1 + y2) / 2) / 224).item(), 6)
+                        # # bounding box 중심 좌표
+                        # box_center_x = (box[0] + box[2]) / 2
+                        # box_center_y = (box[1] + box[3]) / 2
+                        
+                        # 이미지 중심과 bounding box 중심 거리 계산
+                        distance_to_center = np.sqrt((img_center_x - box_center_x) ** 2 + (img_center_y - box_center_y) ** 2)
+
+                        print("Bounding Box 중심 좌표:", (box_center_x, box_center_y))
+                        print("이미지 중심과의 거리:", distance_to_center)
+                else:
+                    print("감지된 객체 없음")
+                
+                
+            # cv2.destroyAllWindows()
+            
+            # 이미지를 로컬 디렉토리에 저장
+            # filename = "received_image.jpg"  # 저장할 파일명
+            # cv2.imwrite(filename, img)
+            # print(f"이미지가 {filename}으로 저장되었습니다.")
+
+async def main():
+    await receive_image()
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
+import numpy as np
+import cv2
+import base64
+import socket
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+# IP 주소
+host_ip = socket.gethostbyname(socket.gethostname())
 
-# 웹소켓 이벤트 핸들러
-@socketio.on('image')
-def handle_image(image):
-    # 클라이언트로부터 받은 이미지 데이터 처리
-    # 이 예시에서는 클라이언트로부터 받은 이미지를 다시 클라이언트에게 전송하여 화면에 표시합니다.
-    emit('stream', image, broadcast=True)
-
-# 기본 경로
 @app.route('/')
 def index():
+    print("페이지 접속완료")
     return render_template('index.html')
 
+@socketio.on('image')
+def handle_image(data):
+    # base64로 인코딩된 이미지 데이터 디코딩
+    image_data = base64.b64decode(data.split(',')[1])
+    nparr = np.fromstring(image_data, np.uint8)
+    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # 이미지 처리 등을 수행할 수 있음
+    # 예시: 이미지를 그대로 웹 페이지에 전송
+    _, img_encoded = cv2.imencode('.jpg', img_np)
+    img_as_text = base64.b64encode(img_encoded).decode('utf-8')
+    socketio.emit('image_processed', img_as_text)
+
 if __name__ == '__main__':
-    # 서버 실행
-    socketio.run(app, host='0.0.0.0', port=5000)
+    print("서버 실행 완료")
+    print("IP주소 :", host_ip)
+    socketio.run(app, host='192.168.116.165', port=5000, debug=True)
